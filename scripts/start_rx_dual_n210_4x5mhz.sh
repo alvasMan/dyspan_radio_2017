75d68d4b33067ee02755fa4c65d4ae9f5243ff9d
Andre Puschmann <andre.puschmann@tu-ilmenau.de>
2015-09-01 09:32:28 +0200

    add new start script for receiver

---
+++ scripts/start_rx_dual_n210_4x5mhz.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+BUILD_DIR=../build_release/src
+cd $BUILD_DIR
+./dyspanradio --mode=rx --freq=2.49e9 --channel_bandwidth=5000000 --rxgain=25 --num_channels=4 --args="addr0=192.168.70.2,addr1=192.168.70.3,recv_frame_size=4000" --subdev="A:0 A:0"
