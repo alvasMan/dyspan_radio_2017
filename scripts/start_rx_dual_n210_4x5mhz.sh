50208226980c6602de495e347f1a13b974d20bd8
Francisco Paisana <paisanaf@tcd.ie>
2015-09-27 16:14:19 +0100

    update rx script

--- scripts/start_rx_dual_n210_4x5mhz.sh
+++ scripts/start_rx_dual_n210_4x5mhz.sh
@@ -2,4 +2,7 @@
 BUILD_DIR=../build_release/src
 cd $BUILD_DIR
 #./dyspanradio --mode=rx --freq=2.49e9 --channel_bandwidth=5000000 --rxgain=25 --num_channels=4 --args="addr0=192.168.70.2,addr1=192.168.70.3,recv_frame_size=4000" --subdev="A:0 A:0"
-./dyspanradio --mode=rx --freq=2.49e9 --channel_bandwidth=5000000 --rxgain=25 --num_channels=4 --args="addr0=192.168.70.2,addr1=192.168.70.3,recv_frame_size=4000" --rxsubdev="A:0 A:0" --challenge=false --db_ip=192.168.1.104 --debug=true
+
+#./dyspanradio --mode=rx --freq=2.49e9 --channel_bandwidth=5000000 --rxgain=25 --num_channels=4 --args="addr0=192.168.70.2,addr1=192.168.70.3,recv_frame_size=4000" --rxsubdev="A:0 A:0" --challenge=false --db_ip=192.168.1.104 --debug=true
+# working in dublin
+./dyspanradio --mode=rx --freq=2.4125e9 --channel_bandwidth=5000000 --rxgain=25 --num_channels=4 --args="addr0=192.168.10.2,addr1=192.168.10.3,recv_frame_size=4000" --rxsubdev="A:0 A:0" --challenge=true --db_ip=192.168.2.101 --debug=false --numtrx 2
