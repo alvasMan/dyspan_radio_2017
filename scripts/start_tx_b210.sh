cf38369087b010d3608cd4e658f5676b0f27ec90
Andre Puschmann <andre@softwareradiosystems.com>
2017-01-24 12:57:44 +0100

    update tx script

--- scripts/start_tx_b210.sh
+++ scripts/start_tx_b210.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BUILD_DIR=../build_release/src
 cd $BUILD_DIR
-./dyspanradio --args="master_clock_rate=40e6" --freq=2412500000 --channel_bandwidth=5000000 --channel_rate=5000000 --rxgain=50 --num_channels=4 --txgain_uhd=83 --txgain_soft=-24
+./dyspanradio --freq=2.49e9 --num_channels=4 --txgain_uhd=70 --txgain_soft=-12 --rxgain=1 --challenge=false --debug=false --rxsubdev="A:B" --txsubdev="A:A" --mod=qpsk --crc crc16 --sensing=false
